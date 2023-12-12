6bda3b15a512b6be1fb8e7a5527f93d87306e34e
SerafimovaD <daci.serafimova@gmail.com>
2023-12-12 13:44:57 +0100

    Add first java program

---
+++ src/proektt_13/MyClass.java
@@ -0,0 +1,12 @@
+package proektt_13;
+
+public class MyClass {
+	public static void main(String[] args) {
+	Vehiclee v1 = new Vehiclee(); // Using default color "Blue"
+	Vehiclee v2 = new Vehiclee("Red"); // Setting color to "Red"
+	        
+	System.out.println("Vehiclee color: " + v1); // Prints "Vehicle color: Blue"
+    System.out.println("Vehiclee color: " + v2); // Prints "Vehicle color: Red"
+	    }
+	}
+
---
+++ src/proektt_13/Vehiclee.java
@@ -0,0 +1,19 @@
+package proektt_13;
+
+public class Vehiclee {
+	    private String color;
+
+	    // Constructor with a default color
+	    Vehiclee() {
+	        color = "Blue"; // Set a default color, e.g., "Blue"
+	    }
+
+	    // Constructor with a parameter to set the color
+	    Vehiclee(String a) {
+	        color = a;
+	    }
+	    public String toString() {
+	    return color;
+	}
+}
+
